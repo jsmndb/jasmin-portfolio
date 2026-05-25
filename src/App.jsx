734c8916291158164e894af3dbc429fda0b721e7
jsmndb <jasmindbernal@gmail.com>
2026-05-25 15:25:23 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Jasmin</h1>
+      <p>My first React portfolio website.</p>
+    </div>
+  );
+}
+
+export default App;
